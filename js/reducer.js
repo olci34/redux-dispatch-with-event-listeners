c9abf0eb7749d84bb8a4092fe6cf73ac9629ae01
github username <github email address>
2021-05-25 01:18:01 -0400

    commit tree 75275e483bfce182ef7c423f089c6757676cfedc

--- js/reducer.js
+++ js/reducer.js
@@ -1,1 +1,23 @@
 // add code snippets from README
+let state;
+
+function changeState(state={count: 0}, action){
+    switch (action.type) {
+        case ('INCREASE_COUNT'):
+            return {count: state.count + 1};
+        default:
+            return state;
+    }
+}
+
+function dispatch(action) {
+    state = changeState(state, action)
+    render()
+}
+
+function render() {
+    document.getElementById('container').textContent = state.count
+}
+
+document.querySelector('button').addEventListener('click', () => dispatch({type: 'INCREASE_COUNT'}))
+dispatch({type: '@@INIT' })
